208f4838f04ce99715b106f1f9e96493ae448d21
mmulko <mykhailo.mulko@ucu.edu.ua>
2021-04-17 00:33:39 +0300

    Add Flask

--- youtube_helper.py
+++ youtube_helper.py
@@ -8,7 +8,7 @@ import youtube_dl
 def download_audio(link, directory):
     ydl_opts = {
         'format': 'bestaudio/best',
-        'outtmpl': f"{directory}/%(extractor_key)s/%(extractor)s-%(id)s-%(title)s.%(ext)s",
+        'outtmpl': f'{directory}/audio.%(ext)s',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'wav',
